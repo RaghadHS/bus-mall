0b502af755764418938218a74353123e4f143f90
RaghadHS <raghad.alsawalhi@gmail.com>
2019-11-25 17:52:46 +0200

    working

--- app.js
+++ app.js
@@ -22,55 +22,85 @@ var imagePaths = ['img/bag.jpg',
   'img/water-can.jpg',
   'img/wine-glass.jpg'];
 
+var names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast',
+  'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 
+  'scissors', 'shark' , 'sweep', 'tauntaun', 'unicorn','usb', 'water-can', 'wine-glass'];
+
+  
 // ////Constructor of Products
 ////Assigns name of product and image file 
-function Product() {
+function Product(names, paths) {
 
-  var name;
-  var path;
+  var productName = names;
+  var productPath = paths;
 
-  var numClicked = 0;
+
+  var views;
+  this.views++;
+  var numClicked;
+  this.numClicked++;
   ///////FIX ADDING NUMBER TO numClicked
 
   this.displayThreeItems();
   this.eventListen();
 }
 
+var right = Math.ceil(Math.random() * imagePaths.length - 1);
+var middle = Math.ceil(Math.random() * imagePaths.length - 1);
+var left = Math.ceil(Math.random() * imagePaths.length - 1);
 
 Product.prototype.displayThreeItems = function(){
 
+    
   ////Generating 3 random numbers to choose images from array
-  var right = Math.ceil(Math.random() * imagePaths.length);
-  var middle = Math.ceil(Math.random() * imagePaths.length);
-  var left = Math.ceil(Math.random() * imagePaths.length);
+  right = Math.ceil(Math.random() * imagePaths.length - 1);
+  middle = Math.ceil(Math.random() * imagePaths.length - 1);
+  left = Math.ceil(Math.random() * imagePaths.length - 1);
 
   while( right === left || right === middle || left === middle)
   {
-    right = Math.ceil(Math.random() * imagePaths.length);
-    middle = Math.ceil(Math.random() * imagePaths.length);
-    left = Math.ceil(Math.random() * imagePaths.length);
+    right = Math.ceil(Math.random() * imagePaths.length - 1);
+    middle = Math.ceil(Math.random() * imagePaths.length - 1);
+    left = Math.ceil(Math.random() * imagePaths.length - 1);
 
   }
 
+
   var rightImg = document.getElementById('right');
   rightImg.setAttribute('src', imagePaths[right]);
+  rightImg.setAttribute('alt', names[right]);
 
   var middleImg = document.getElementById('middle');
   middleImg.setAttribute('src', imagePaths[middle]);
+  middleImg.setAttribute('alt', names[middle]);
 
   var leftImg = document.getElementById('left');
   leftImg.setAttribute('src', imagePaths[left]);
-
+  leftImg.setAttribute('alt', imagePaths[left]);
 
   
 };
 
+var leftListener = document.getElementById('left');
+var middleListener = document.getElementById('middle');
+var rightListener = document.getElementById('right');
 
-Product.prototype.eventListen = function()
-{
+///////Event Listener for the Right Image
+rightListener.addEventListener('click', function(){
+
+  new Product(names[right], imagePaths[right]);
+});
 
 
-};
+///////Event Listener for the Middle Image
+middleListener.addEventListener('click', function(){
 
+  new Product(names[middle], imagePaths[middle]);
+});
 
-var trial = new Product();
+////////Event Listener for the Left Image
+leftListener.addEventListener('click', function(){
+
+  new Product(names[left], imagePaths[left]);
+});
+new Product();
